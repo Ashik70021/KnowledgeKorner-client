f25ed5e41bf6000e79555a05b8f4bb43c7145f89
Ashik70021 <ashik70021@gmail.com>
2024-05-10 02:21:22 +0600

    handle firebase config keys

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCF1hRB7zVJYSVGxVtRN7snhXlLWfxoKxA",
-  authDomain: "knowledge-korner.firebaseapp.com",
-  projectId: "knowledge-korner",
-  storageBucket: "knowledge-korner.appspot.com",
-  messagingSenderId: "257917079517",
-  appId: "1:257917079517:web:eafb061d8132ebd5aaf034"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
