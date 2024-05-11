b9bc08fbba6016a12f41bf34caec9e844696696f
Ashik70021 <ashik70021@gmail.com>
2024-05-11 21:51:25 +0600

    create blog details page

--- src/Pages/BlogDetails.jsx
+++ src/Pages/BlogDetails.jsx
@@ -6,37 +6,166 @@ const BlogDetails = () => {
     const item = items.find(item => item._id === id)
     console.log(item, id)
     return (
-        <div className="my-16 grid grid-cols-12 container mx-auto">
-            {/* left side */}
-            <div className="col-span-9 bg-red-500">
-                <div className=" p-4 shadow-md bg-gray-50 text-gray-800">
-                    <div className="flex justify-between pb-4 border-bottom">
-                        <div className="flex items-center">
-                            <a rel="noopener noreferrer" href="#" className="mb-0 capitalize text-gray-800">{item.category}</a>
+        <div>
+            <div className="my-16 grid grid-cols-12 container mx-auto">
+                {/* left side */}
+                <div className="col-span-9 bg-red-500">
+                    <div className=" p-4 shadow-md bg-gray-50 text-gray-800">
+                        <div className="flex justify-between pb-4 border-bottom">
+                            <div className="flex items-center">
+                                <a rel="noopener noreferrer" href="#" className="mb-0 capitalize text-gray-800">{item.category}</a>
+                            </div>
+                            <a rel="noopener noreferrer" href="#">See All</a>
                         </div>
-                        <a rel="noopener noreferrer" href="#">See All</a>
-                    </div>
-                    <div className="space-y-4">
-                        <div className="space-y-2">
-                            <img src="https://source.unsplash.com/random/480x360/?4" alt="" className="block object-cover object-center w-full rounded-md h-[500px] bg-gray-500" />
-                            <div className="flex items-center text-xs">
-                                <span>6 min ago</span>
+                        <div className="space-y-4">
+                            <div className="space-y-2">
+                                <img src="https://source.unsplash.com/random/480x360/?4" alt="" className="block object-cover object-center w-full rounded-md h-[500px] bg-gray-500" />
+                                <div className="flex items-center text-xs">
+                                    <span>6 min ago</span>
+                                </div>
+                            </div>
+                            <div className="space-y-2">
+                                <a rel="noopener noreferrer" href="#" className="block">
+                                    <h3 className="text-2xl font-semibold text-cyan-600">{item.title}</h3>
+                                </a>
+                                <p className="text-xl leading-snug text-gray-600">{item.description}</p>
                             </div>
                         </div>
-                        <div className="space-y-2">
-                            <a rel="noopener noreferrer" href="#" className="block">
-                                <h3 className="text-2xl font-semibold text-cyan-600">{item.title}</h3>
-                            </a>
-                            <p className="text-xl leading-snug text-gray-600">{item.description}</p>
+                    </div>
+                </div>
+
+                {/* right side */}
+                <div className="col-span-3">
+                    <div className="mt-8 mx-auto max-w-xs p-6 shadow-md rounded-xl sm:px-12 bg-gray-50 text-gray-800">
+                        <img src="https://source.unsplash.com/150x150/?portrait?3" alt="" className="w-32 h-32 mx-auto rounded-full bg-gray-500 aspect-square" />
+                        <div className="space-y-4 text-center divide-y divide-gray-300">
+                            <div className="my-2 space-y-1">
+                                <h2 className="text-xl font-semibold sm:text-2xl">Leroy Jenkins</h2>
+                                <p className="px-5 text-xs sm:text-base text-gray-600">Full-stack developer</p>
+                            </div>
+                            <div className="flex justify-center pt-2 space-x-4 align-center">
+                                <a rel="noopener noreferrer" href="#" aria-label="GitHub" className="p-2 rounded-md text-gray-800 hover:text-cyan-600">
+                                    <svg viewBox="0 0 496 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
+                                        <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
+                                    </svg>
+                                </a>
+                                <a rel="noopener noreferrer" href="#" aria-label="Dribble" className="p-2 rounded-md text-gray-800 hover:text-cyan-600">
+                                    <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
+                                        <path d="M256 8C119.252 8 8 119.252 8 256s111.252 248 248 248 248-111.252 248-248S392.748 8 256 8zm163.97 114.366c29.503 36.046 47.369 81.957 47.835 131.955-6.984-1.477-77.018-15.682-147.502-6.818-5.752-14.041-11.181-26.393-18.617-41.614 78.321-31.977 113.818-77.482 118.284-83.523zM396.421 97.87c-3.81 5.427-35.697 48.286-111.021 76.519-34.712-63.776-73.185-116.168-79.04-124.008 67.176-16.193 137.966 1.27 190.061 47.489zm-230.48-33.25c5.585 7.659 43.438 60.116 78.537 122.509-99.087 26.313-186.36 25.934-195.834 25.809C62.38 147.205 106.678 92.573 165.941 64.62zM44.17 256.323c0-2.166.043-4.322.108-6.473 9.268.19 111.92 1.513 217.706-30.146 6.064 11.868 11.857 23.915 17.174 35.949-76.599 21.575-146.194 83.527-180.531 142.306C64.794 360.405 44.17 310.73 44.17 256.323zm81.807 167.113c22.127-45.233 82.178-103.622 167.579-132.756 29.74 77.283 42.039 142.053 45.189 160.638-68.112 29.013-150.015 21.053-212.768-27.882zm248.38 8.489c-2.171-12.886-13.446-74.897-41.152-151.033 66.38-10.626 124.7 6.768 131.947 9.055-9.442 58.941-43.273 109.844-90.795 141.978z"></path>
+                                    </svg>
+                                </a>
+                                <a rel="noopener noreferrer" href="#" aria-label="Twitter" className="p-2 rounded-md text-gray-800 hover:text-cyan-600">
+                                    <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
+                                        <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
+                                    </svg>
+                                </a>
+                                <a rel="noopener noreferrer" href="#" aria-label="Email" className="p-2 rounded-md text-gray-800 hover:text-cyan-600">
+                                    <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
+                                        <path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mt-14">
+                        <div className="max-w-[83%] p-8 mx-auto rounded-xl bg-gray-50 text-gray-800">
+                            <div className="flex justify-between space-x-8">
+                                <div className="flex flex-col items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-24 h-24 p-2 text-yellow-400 fill-current">
+                                        <path d="M256,104c-83.813,0-152,68.187-152,152s68.187,152,152,152,152-68.187,152-152S339.813,104,256,104Zm0,272A120,120,0,1,1,376,256,120.136,120.136,0,0,1,256,376Z"></path>
+                                        <rect width="32" height="48" x="240" y="16"></rect>
+                                        <rect width="32" height="48" x="240" y="448"></rect>
+                                        <rect width="48" height="32" x="448" y="240"></rect>
+                                        <rect width="48" height="32" x="16" y="240"></rect>
+                                        <rect width="32" height="45.255" x="400" y="393.373" transform="rotate(-45 416 416)"></rect>
+                                        <rect width="32.001" height="45.255" x="80" y="73.373" transform="rotate(-45 96 96)"></rect>
+                                        <rect width="45.255" height="32" x="73.373" y="400" transform="rotate(-45.001 96.002 416.003)"></rect>
+                                        <rect width="45.255" height="32.001" x="393.373" y="80" transform="rotate(-45 416 96)"></rect>
+                                    </svg>
+                                    <h1 className="text-xl font-semibold">Stockholm</h1>
+                                </div>
+                                <span className="font-bold text-8xl">14°</span>
+                            </div>
+                            <div className="flex justify-between mt-8 space-x-4 text-gray-600">
+                                <div className="flex flex-col items-center space-y-1">
+                                    <span className="uppercase">wed</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-8 h-8 fill-current">
+                                        <path d="M398.2,137.208a144.013,144.013,0,0,0-284.545.979,122.364,122.364,0,0,0-64.357,32.926A109.4,109.4,0,0,0,16,249.619c0,31.119,12.789,60.762,36.01,83.469q2.84,2.776,5.845,5.347l11.327-33.981C56.091,289.3,48,270.017,48,249.619c0-42.362,35.724-78.015,81.329-81.168l14.055-.972.814-14.065a111.995,111.995,0,0,1,223.589-.22l.891,14.888,14.913.155c46.592.488,80.409,34.714,80.409,81.382,0,33.152-16.706,61.38-41.84,75.9L409.032,364.9a110.012,110.012,0,0,0,54.938-32.358C484.625,310.339,496,280.889,496,249.619,496,190.507,454.859,144.4,398.2,137.208Z"></path>
+                                        <polygon points="126.029 496 159.817 496 223.153 309.136 192.847 298.864 126.029 496"></polygon>
+                                        <polygon points="294.029 496 327.817 496 391.153 309.136 360.847 298.864 294.029 496"></polygon>
+                                        <polygon points="290.11 251.033 225.781 448 259.445 448 320.529 260.967 290.11 251.033"></polygon>
+                                        <polygon points="128.791 251.033 64.461 448 98.125 448 159.209 260.967 128.791 251.033"></polygon>
+                                    </svg>
+                                    <span>11°</span>
+                                </div>
+                                <div className="flex flex-col items-center space-y-1">
+                                    <span className="uppercase">thu</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-8 h-8 fill-current">
+                                        <path d="M382.76,432H136c-30.732,0-61.371-12.725-84.061-34.912-23.221-22.707-36.009-52.35-36.009-83.469A109.4,109.4,0,0,1,49.136,235.2a122.281,122.281,0,0,1,62.794-32.707V200c0-79.4,64.6-144,144-144s144,64.6,144,144v1.453c55.716,7.939,96,53.729,96,112.166,0,31.27-11.375,60.72-32.031,82.927A109.747,109.747,0,0,1,382.76,432ZM255.93,88a112.127,112.127,0,0,0-112,112v31.405l-14.864,1.059c-45.5,3.239-81.136,38.887-81.136,81.155C47.93,359.635,89.084,400,136,400H382.76c45.515,0,81.17-37.943,81.17-86.381,0-46.566-33.731-80.791-80.2-81.379l-15.8-.2V200A112.127,112.127,0,0,0,255.93,88Z"></path>
+                                    </svg>
+                                    <span>17°</span>
+                                </div>
+                                <div className="flex flex-col items-center space-y-1">
+                                    <span className="uppercase">fri</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-8 h-8 fill-current">
+                                        <path d="M399.667,264.875v-3.813c0-79.4-64.6-144-144-144-2.2,0-4.391.057-6.569.156A116.689,116.689,0,1,0,112.315,247.444c-.422,4.484-.648,9.025-.648,13.618v3.813A116.633,116.633,0,0,0,132.287,496.3H379.046a116.633,116.633,0,0,0,20.621-231.427ZM48.75,132.688a84.677,84.677,0,0,1,168.705-10.47,144.606,144.606,0,0,0-98.59,93.876A84.807,84.807,0,0,1,48.75,132.688ZM379.046,464.3H132.287a84.619,84.619,0,0,1-3.9-169.148l15.277-.69v-33.4a112,112,0,1,1,224,0v33.4l15.277.69a84.619,84.619,0,0,1-3.9,169.148Z"></path>
+                                    </svg>
+                                    <span>8°</span>
+                                </div>
+                                <div className="flex flex-col items-center space-y-1">
+                                    <span className="uppercase">sat</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-8 h-8 fill-current">
+                                        <polygon points="135.279 206.728 224.136 258.323 144.097 304.797 48.308 279.129 48.308 312.259 123.155 332.313 102.99 407.571 131.68 424.135 155.518 335.169 240 286.115 240 374.402 168.823 445.579 197.513 462.144 258 401.657 317.135 460.792 345.826 444.228 272 370.402 272 286.115 355.002 334.31 379.279 424.914 407.97 408.349 387.596 332.313 464 311.841 464 278.712 367.508 304.567 287.864 258.323 376.327 206.957 464 230.449 464 197.32 394.346 178.657 413.576 106.888 384.886 90.323 361.196 178.739 272 230.53 272 130.568 338.833 63.735 310.143 47.171 254.971 102.343 200.664 48.037 171.974 64.601 240 132.627 240 230.53 149.325 177.88 126.073 91.103 97.382 107.667 116.404 178.657 48.308 196.903 48.308 230.032 135.279 206.728"></polygon>
+                                    </svg>
+                                    <span>-2°</span>
+                                </div>
+                                <div className="flex flex-col items-center space-y-1">
+                                    <span className="uppercase">sun</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-8 h-8 fill-current">
+                                        <path d="M382.76,432H136c-30.732,0-61.371-12.725-84.061-34.912-23.221-22.707-36.009-52.35-36.009-83.469A109.4,109.4,0,0,1,49.136,235.2a122.281,122.281,0,0,1,62.794-32.707V200c0-79.4,64.6-144,144-144s144,64.6,144,144v1.453c55.716,7.939,96,53.729,96,112.166,0,31.27-11.375,60.72-32.031,82.927A109.747,109.747,0,0,1,382.76,432ZM255.93,88a112.127,112.127,0,0,0-112,112v31.405l-14.864,1.059c-45.5,3.239-81.136,38.887-81.136,81.155C47.93,359.635,89.084,400,136,400H382.76c45.515,0,81.17-37.943,81.17-86.381,0-46.566-33.731-80.791-80.2-81.379l-15.8-.2V200A112.127,112.127,0,0,0,255.93,88Z"></path>
+                                    </svg>
+                                    <span>4°</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            {/* right side */}
-            <div className="col-span-3 bg-blue-600">
-                <h1>small</h1>
+
+            {/* Comment Section */}
+            <div className="my-16 container mx-auto">
+                <h1 className="text-xl font-semibold my-4">Comments</h1>
+                <form >
+                    <textarea placeholder="Bio" className="textarea textarea-bordered textarea-md w-2/4" ></textarea>
+                    <div>
+                        <button className="py-2 px-4 border rounded-lg bg-gray-500 text-white font-medium">Submit</button>
+                    </div>
+                </form>
+
+                <div className="my-6 w-2/4 border-b-2">
+                    <div className="flex items-center">
+                        <img className="object-cover h-10 rounded-full" src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=48&q=60" alt="Avatar" />
+                        <a href="#" className="mx-2 font-semibold text-gray-700 dark:text-gray-200" role="link">Jone Doe</a>
+                    </div>
+                    <div className="mt-2">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eveniet assumenda quas officia, rem et.</p>
+                    </div>
+                    <p className="my-1">6 min ago</p>
+                </div>
+                <div className="my-6 w-2/4 border-b-2">
+                    <div className="flex items-center">
+                        <img className="object-cover h-10 rounded-full" src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=48&q=60" alt="Avatar" />
+                        <a href="#" className="mx-2 font-semibold text-gray-700 dark:text-gray-200" role="link">Jone Doe</a>
+                    </div>
+                    <div className="mt-2">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eveniet assumenda quas officia, rem et.</p>
+                    </div>
+                    <p className="my-1">6 min ago</p>
+                </div>
             </div>
         </div>
+
     );
 };
 
