09fd4942871a7ea4468eb5c9a91e8a1197c77cbc
Ashik70021 <ashik70021@gmail.com>
2024-05-14 17:17:09 +0600

    implement populer bloger section

--- src/Components/PopulerBlogger.jsx
+++ src/Components/PopulerBlogger.jsx
@@ -64,8 +64,7 @@ const PopulerBlogger = () => {
                                 <a rel="noopener noreferrer" href="#" className="text-md tracking-wider uppercase hover:underline text-fuchsia-600">CREATIVITY & INSPIRATION</a>
                                 <h3 className="flex-1 py-2 text-xl font-semibold leading-snug">Liam Taylor</h3>
                                 <div className="flex flex-wrap justify-between pt-3 space-x-2 text-md text-gray-600">
-                                    <span>June 4, 2020</span>
-                                    <span>2.4K views</span>
+                                    
                                 </div>
                             </div>
                         </article>
